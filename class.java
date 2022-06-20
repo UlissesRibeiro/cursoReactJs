9ee1f873ec14a63ce3f211347e966e770163720d
ulisses gomes <ulissestark@gmail.com>
2022-06-19 22:07:54 -0300

    operations arrays

---
+++ class.java
@@ -0,0 +1,18 @@
+public class funcionarios{
+    char nome;
+    int matricula;
+    int cpf;
+    int dataNascimento;
+
+    public pessoa(string nome,int matricula,int cpf, int dataNascimento){
+        this.Nome=nome;
+        this.Matricula = matricula;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+pessoa pessoa1 = new pessoa("joao",12345,12345678901,21081992);
+
+
+
+}
+
